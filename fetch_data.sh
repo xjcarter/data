ba59cbdd3711af1f3acd00f768f4acadd06720aa
James Carter <xjcarter@gmail.com>
2023-04-25 21:12:18 -0400

    added fetch_data.sh

---
+++ fetch_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /home/jcarter/sandbox/trading/env.sh
+home="/home/jcarter/sandbox/trading/data/"
+
+echo
+if [ $# -lt 1 ]
+then
+    echo "Uaage: fetch_data.sh <fetch_list_file>"
+else
+    fetch_file="${home}/${1}"
+    if [ !-d ${fetch_file} ]
+    then
+        echo "fetch_list_file: ${fetch_file} not found."
+        exit 1
+    else
+        echo "Using fetch_file ${fetch_file} ..."
+        python ${home}/yahoo_data.py --file="${fetch_file}" 
+        python ${home}/data_catalog.py --file="${fetch_file}" 
+    fi
+fi
+
+
+
